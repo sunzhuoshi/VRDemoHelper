dd3b23f4bff818fb470e594c04ac9f5dcb29d1c8
sunzhuoshi <sunzhuoshi@gmail.com>
2017-03-31 18:56:43 +0800

    Fixed wrong code with shared pointer

--- VRDemoHelper/VRDemoCoreWrapper.h
+++ VRDemoHelper/VRDemoCoreWrapper.h
@@ -1,9 +1,11 @@
 #pragma once
 #include <VRDemoEventDispatcher.h>
+#include <log4cplus\helpers\pointer.h>
 
 class VRDemoCoreWrapper : public IVRDemoEventListener
 {
-public:
+public:   
+    typedef log4cplus::helpers::SharedObjectPtr<VRDemoCoreWrapper> VRDemoCoreWrapperPtr;
     VRDemoCoreWrapper();
     ~VRDemoCoreWrapper();
     bool init(BOOL trace);
