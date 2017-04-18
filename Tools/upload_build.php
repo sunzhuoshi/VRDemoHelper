992051c67fe0b259f48a48deb299b8262823f534
sunzhuoshi <sunzhuoshi@gmail.com>
2017-04-18 21:32:41 +0800

    Fix syntex errors

--- Tools/upload_build.php
+++ Tools/upload_build.php
@@ -10,17 +10,18 @@ function respond($code, $message) {
     
 try {
     if ('PUT' == $_SERVER('REQUEST_METHOD')) {
-        if (isset($_GET['build_file'])) {
-            // remove folders if any(security reason)
+		if (isset($_GET['build_file'])) {
+			// remove folders if any(security reason)
             $splitted = preg_split('/[/\\\\]/', $_GET['build_file']);
-            $c = count($splitted);
-                
-            if ($c) {
-                $build_file = $splitted[$c - 1];
+            $c = count($splitted);            
+			if ($c) {
+	            $build_file = $splitted[$c - 1];
                 $build_file_path = 'builds/' . $build_file;
-                if (!file_exists($build_file_path)) {
-                    $fp = fopen('builds/' . $build_file, 'w');
-                    while ($data = fread($putdata, 1024)) {
+                if (!file_exists($build_file_path)) 
+				{
+				    $fp = fopen('builds/' . $build_file, 'w');
+                    while ($data = fread($putdata, 1024)) 
+					{
                         fwrite($fp, $data);
                     }
                     fclose($fp);
@@ -30,14 +31,15 @@ try {
                 else {
                     respond(400, 'Bad Request: file already exists');
                 }
-            }
-            else {
+            } 
+			else {
                 respond(400, 'Bad Request: invalid parameter value');
             }
-        }
-        else {
+		} 
+		else {
             respond(400, 'Bad Request: build_file missed');
         }
+	}
     else {
         respond(400, 'Bad Request: http method unsupported');
     }
