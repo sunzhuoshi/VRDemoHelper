4212a291c238f04fb49484b480a265b6af07222f
sunzhuoshi <sunzhuoshi@gmail.com>
2017-04-20 00:02:36 +0800

    Clearn some dll export code and settings

--- VRDemoCore/VRDemoCore.h
+++ VRDemoCore/VRDemoCore.h
@@ -1,10 +1,4 @@
-#ifdef VRDEMOCORE_EXPORTS
-#define VRDEMOCORE_API __declspec(dllexport)
-#else
-#define VRDEMOCORE_API __declspec(dllimport)
-#endif
-
-#include "VRDemoArbiter.h"
+#pragma once
 
 extern "C" LRESULT WINAPI fnWndMsgProc(INT nCode, WPARAM wParam, LPARAM lParam);
 
